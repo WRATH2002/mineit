98052348a7e88581f1f7102e6c57f99954f98927
Himadri Purkait <purkaithimadricollege@gmail.com>
2024-03-12 13:39:52 +0530

    2nd

--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-const REACT_APP_API_KEY = "AIzaSyD0sVyOwytM73oc0DceenfxH - q8X_WYtpM";
-const REACT_APP_BOOK_API =
-  "GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=" +
-  REACT_APP_API_KEY;
